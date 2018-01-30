65e51dc1e000280d3a1dc98023df3c5e817d8514
Colin Grambow <cgrambow@mit.edu>
2018-01-29 20:40:36 -0500

    Add default rules for bimolecular families with single tree
    RMG does not average rates correctly if the only data available is for
    identical nodes in bimolecular families where both trees are identical.
    Instead, it tries to fall back up to the topmost node and will throw an
    error if this node has no data available.

--- input/kinetics/families/Peroxyl_Termination/rules.py
+++ input/kinetics/families/Peroxyl_Termination/rules.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-name = "Bimolec_Hydroperoxide_Decomposition/rules"
+name = "Peroxyl_Termination/rules"
 shortDesc = u""
 longDesc = u"""
 
 """
 entry(
     index = 0,
-    label = "ROOH;ROOH",
+    label = "RHOO_rad;ROO_rad",
     kinetics = ArrheniusEP(
-        A = (1.096e5, 'm^3/(mol*s)'),
-        n = 0,
+        A = (2e+11, 'cm^3/(mol*s)'),
+        n = -0.55,
         alpha = 0,
-        E0 = (96.1, 'kJ/mol'),
+        E0 = (-1600, 'cal/mol'),
         Tmin = (300, 'K'),
         Tmax = (1500, 'K'),
     ),
--- input/kinetics/families/PriSec_Peroxyl_Disproportionation/rules.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-name = "PriSec_Peroxyl_Disproportionation/rules"
-shortDesc = u""
-longDesc = u"""
-
-"""
